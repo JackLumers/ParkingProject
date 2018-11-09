b891cd79490517a1d04f2cdd0e2881ffba2c80bf
ilnazMullin <35495856+ilnazMullin@users.noreply.github.com>
2018-11-09 20:10:55 +0300

    Добавлено окно авторизации
    кек

--- src/mainStruct/Constants.java
+++ src/mainStruct/Constants.java
@@ -5,5 +5,7 @@ final class Constants {
     //Window options
     static final double MIN_WINDOW_HEIGHT = 500;
     static final double MIN_WINDOW_WIDTH = 700;
+    static final double AUTORIZATION_WINDOW_WIDTH = 100;
+    static final double AUTORIZATION_WINDOW_HEIGHT = 40;
 
 }
