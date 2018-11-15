b107e0d1575d9e64b4f3620d4cfbf2f65bc7a711
ilnazMullin <35495856+ilnazMullin@users.noreply.github.com>
2018-11-15 16:17:46 +0300

    Соеденение с БД Oracle XE

---
+++ src/mainStruct/Client.java
@@ -0,0 +1,91 @@
+package mainStruct;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Client {
+    //Declare Employees Table Columns
+    private IntegerProperty employee_id;
+    private StringProperty first_name;
+    private StringProperty last_name;
+    private StringProperty email;
+    private StringProperty phone_number;
+
+
+    //Constructor
+    public Client() {
+        this.employee_id = new SimpleIntegerProperty();
+        this.first_name = new SimpleStringProperty();
+        this.last_name = new SimpleStringProperty();
+        this.email = new SimpleStringProperty();
+        this.phone_number = new SimpleStringProperty();
+    }
+
+    //employee_id
+    public int getEmployeeId() {
+        return employee_id.get();
+    }
+
+    public void setEmployeeId(int employeeId){
+        this.employee_id.set(employeeId);
+    }
+
+    public IntegerProperty employeeIdProperty(){
+        return employee_id;
+    }
+
+    //first_name
+    public String getFirstName () {
+        return first_name.get();
+    }
+
+    public void setFirstName(String firstName){
+        this.first_name.set(firstName);
+    }
+
+    public StringProperty firstNameProperty() {
+        return first_name;
+    }
+
+    //last_name
+    public String getLastName () {
+        return last_name.get();
+    }
+
+    public void setLastName(String lastName){
+        this.last_name.set(lastName);
+    }
+
+    public StringProperty lastNameProperty() {
+        return last_name;
+    }
+
+    //email
+    public String getEmail () {
+        return email.get();
+    }
+
+    public void setEmail (String email){
+        this.email.set(email);
+    }
+
+    public StringProperty emailProperty() {
+        return email;
+    }
+
+    //phone_number
+    public String getPhoneNumber () {
+        return phone_number.get();
+    }
+
+    public void setPhoneNumber (String phoneNumber){
+        this.phone_number.set(phoneNumber);
+    }
+
+    public StringProperty phoneNumberProperty() {
+        return phone_number;
+    }
+
+}
